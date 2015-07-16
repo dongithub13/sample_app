7d626477e415b412163bbe7ff573652f7044f326
Donald Brown <devgumdoe13@gmail.com>
2015-07-16 11:07:59 -0400

    Add password resets & email configuration

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,5 +1,5 @@
 
-  ## Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+  # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at
@@ -13,7 +13,10 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at
   # http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
+
 
